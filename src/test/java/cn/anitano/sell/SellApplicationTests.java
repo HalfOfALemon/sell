3ed70b277c10ae298677350d3594e24a880b607c
HalfOfALemon <1135239973@qq.com>
2019-10-22 17:16:22 +0800

    Initial commit

---
+++ src/test/java/cn/anitano/sell/SellApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.anitano.sell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
